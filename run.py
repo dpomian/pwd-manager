35aff1958724da230528facc5731dcc9da8f5c10
Dorin Pomian <dorin.pomian@gmail.com>
2024-11-22 21:00:04 +0100

    generate password functionality

--- run.py
+++ run.py
@@ -1,6 +1,17 @@
+import argparse
 from pwd_manager import create_app
 
+parser = argparse.ArgumentParser(description="Run the Password Manager app")
+parser.add_argument(
+    "-p",
+    "--port",
+    type=int,
+    default=5000,
+    help="Port to run the app on (default: 5000)",
+)
+
+args = parser.parse_args()
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, port=args.port)
